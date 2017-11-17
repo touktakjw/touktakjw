74124a371b6a794ce1beb898df98ad639b2b7dcc
touk <phonephetvilaysack@gmail.com>
2017-11-17 10:35:18 +0700

    add all file ja

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(".header-subtitle").on("click",function(){
+		$(".call-to-action").slideToggle();
+	});
+
+	$(".call-button").on("click",function(e){
+		e.preventDefault();
+		$(".column img").toggleClass("bigger");
+	});
+
+});
